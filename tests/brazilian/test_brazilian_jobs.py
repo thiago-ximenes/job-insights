288c7792f631b93f73fbf19619a6d220fa79716e
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-08-07 22:18:14 -0300

    pass requirimet 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,15 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path_to_file = "tests/mocks/brazilians_jobs.csv"
+    new_dict = read_brazilian_file(path_to_file)
+    for key in new_dict:
+        assert "title" in key
+        assert "titulo" not in key
+
+        assert "salary" in key
+        assert "salario" not in key
+
+        assert "type" in key
+        assert "tipo" not in key
