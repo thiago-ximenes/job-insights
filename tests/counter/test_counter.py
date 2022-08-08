954227112ffe79c2a1eeb69d5b4f5ffee3a815b1
Thiago Ximenes <thiagoximeneslima@gmail.com>
2022-08-07 21:49:59 -0300

    pass requirimet 9

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    cvs_jobs_file = "src/jobs.cvs"
+
+    assert count_ocurrences(cvs_jobs_file, "Javascript") == 122
+    assert count_ocurrences(cvs_jobs_file, "Python") == 1639
